224038cd0e6f233475161f465c6b80ee249dfe9d
Martin Zagora <zaggino@gmail.com>
2016-09-08 12:34:37 +1000

    store data sent to /api/health

--- config/default.js
+++ config/default.js
@@ -1,6 +1,7 @@
 module.exports = {
   server: {
-    port: 8080
+    port: 8080,
+    requestLimit: '1mb'
   },
   db: {
     url: 'postgres://postgres:postgres@127.0.0.1:5432/brackets-electron-data'
--- src/server/health.ts
+++ src/server/health.ts
@@ -3,9 +3,20 @@ import * as db from './db';
 
 export async function handleHealthGet(ctx: Koa.Context, next: () => Promise<any>) {
   const healthLogs = await db.HealthLogs.findAll();
-  ctx.response.body = healthLogs;
+  ctx.response.body = JSON.stringify(healthLogs, null, 4);
 }
 
 export async function handleHealthPost(ctx: Koa.Context, next: () => Promise<any>) {
-  ctx.response.body = 'hello!';
+  const ip = ctx.request.ip;
+  const userAgent = ctx.request.headers['user-agent'];
+  let data = ctx.request.body;
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data);
+    } catch (err) {
+      /* ignore */
+    }
+  }
+  const healthLog = await db.HealthLogs.create({ ip, userAgent, data });
+  ctx.response.body = JSON.stringify(healthLog, null, 4);
 }
